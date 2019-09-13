d006a8769510bc518ee5be1ad1223114faae0a1a
vedanta-krit.das <vedmalex@gmail.com>
2019-09-13 18:07:43 +0300

    next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { LevelOfConcious, Section } from '../lib/types';
 import { useFetch } from '../lib/useFetch';
@@ -12,64 +11,91 @@ const Index = () => {
   const router = useRouter();
   let n = router.query.number;
   let number: number;
-  if(Array.isArray(n)){
+  if (Array.isArray(n)) {
     number = parseInt(n[0]);
-  } else if(n){
+  } else if (n) {
     number = parseInt(n);
   } else {
     number = 1;
   }
 
-  const [result, setResult] = useState(res)
+  const [result, setResult] = useState(res);
   const [answers, setAnswer] = useState({});
 
-  const [section, loadingSection] = useFetch<Section>(`/api/questions?question=${number-1 || quest}`);
-  const [levelDescription, loadingLevel] = useFetch<LevelOfConcious>(`/api/levels?level=${result || res}`);
+  const [section, loadingSection] = useFetch<Section>(
+    `/api/questions?question=${number - 1 || quest}`,
+  );
+  const [levelDescription, loadingLevel] = useFetch<LevelOfConcious>(
+    `/api/levels?level=${result || res}`,
+  );
   const [level, setLevel] = useState<LevelOfConcious>({} as LevelOfConcious);
-  if(!loadingLevel){
+  if (!loadingLevel) {
     if (level.mark != levelDescription.mark) {
-      setLevel(levelDescription)
+      setLevel(levelDescription);
     }
   }
-  return (<>
-    <h1> Результат теста Хокингса на осознанность: {result} </h1>
-    <ol>
-      <li>Восприятие Бога:{level.whoIsGod}</li>
-      <li>Восприятие жизни: {level.lifeIs}</li>
-      <li>Уровень: {level.level}</li>
-      <li>Оценка: {level.mark}</li>
-      <li>Эмоция: {level.emotion}</li>
-      <li>Процесс: {level.process}</li>
-    </ol>
-    {
-      loadingSection ? ("Loading...") : (
+  return (
+    <>
+      <h1> Результат теста Хокингса на осознанность: {result} </h1>
+      <h4>множественный выбор</h4>
+      <ol>
+        <li>Восприятие Бога:{level.whoIsGod}</li>
+        <li>Восприятие жизни: {level.lifeIs}</li>
+        <li>Уровень: {level.level}</li>
+        <li>Оценка: {level.mark}</li>
+        <li>Эмоция: {level.emotion}</li>
+        <li>Процесс: {level.process}</li>
+      </ol>
+      {loadingSection ? (
+        'Loading...'
+      ) : (
         <>
-          <h2>{number}. {section.name}</h2>
-          {
-            section.questions.map((sq, item) => (
-              <p key={`${number}${item}`}>
-                {item + 1}. {sq.name}
-                <input type="checkbox" onChange={(e) => {
-                  e.target.checked ? setResult((result * 10 + sq.mark) / 10) : setResult((result * 10 - sq.mark) / 10);
-                  e.target.checked? setAnswer({
-                    ...answers,
-                    [`${number}${item}`]:true,
-                  }):
-                  setAnswer({
-                    ...answers,
-                    [`${number}${item}`]:false,
-                  })
-                }} checked={answers[`${number}${item}`]}/>
-                ({sq.state})
+          <h2>
+            {number}. {section.name}
+          </h2>
+          {section.questions.map((sq, item) => (
+            <p key={`${number}${item}`}>
+              {item + 1}. {sq.name}
+              <input
+                type="checkbox"
+                onChange={e => {
+                  e.target.checked
+                    ? setResult((result * 10 + sq.mark) / 10)
+                    : setResult((result * 10 - sq.mark) / 10);
+                  e.target.checked
+                    ? setAnswer({
+                        ...answers,
+                        [`${number}${item}`]: true,
+                      })
+                    : setAnswer({
+                        ...answers,
+                        [`${number}${item}`]: false,
+                      });
+                }}
+                checked={answers[`${number}${item}`]}
+              />
+              ({sq.state})
             </p>
-            ))
-          }
-          {section.hasPrev ? <Link href={`/?number=${number - 1}`}><a>Prev</a></Link> : <a>Prev</a>} {' '}
-          {section.hasNext ? <Link href={`/?number=${number + 1}`}><a>Next</a></Link> : <a>Prev</a>}
-        </>)
-      }
-  </>)
-}
+          ))}
+          {section.hasPrev ? (
+            <Link href={`/?number=${number - 1}`}>
+              <a>Prev</a>
+            </Link>
+          ) : (
+            <a>Prev</a>
+          )}{' '}
+          {section.hasNext ? (
+            <Link href={`/?number=${number + 1}`}>
+              <a>Next</a>
+            </Link>
+          ) : (
+            <a>Prev</a>
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
 // Index.getInitialProps = async ()=>{
 //   const res = await fetch('http://localhost:3000/api/levels');
@@ -85,4 +111,3 @@ const Index = () => {
 // }
 
 export default Index;
-
